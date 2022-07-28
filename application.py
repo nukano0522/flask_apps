3454524ec6adfd8529166be415aff38d51c77f8f
Shuhei Nakano <nukano0522@gmail.com>
2022-07-28 15:59:54 +0900

    gitignoreファイルを追加

--- application.py
+++ application.py
@@ -1,17 +1,19 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET", "POST"])
 def main_page():
-    if request.method == 'GET':
+    if request.method == "GET":
         text = "ここに結果が出力されます"
-        return render_template("page.html",text=text)
-    elif request.method == 'POST':
+        return render_template("page.html", text=text)
+    elif request.method == "POST":
         name = request.form["name"]
         hobby = request.form["hobby"]
-        text = f"こんにちは{name}さん、あなたの趣味は{hobby}なんですね"
-        return render_template("page.html",text=text)
+        text = f"こんにちは、{name}さん、あなたの趣味は{hobby}なんですね"
+        return render_template("page.html", text=text)
+
 
 ## 実行
 if __name__ == "__main__":
