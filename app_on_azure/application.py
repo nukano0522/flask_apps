d761817d5e83c3d2a396b4496f6efa08cea262ab
Shuhei Nakano <nukano0522@gmail.com>
2022-07-29 22:00:45 +0900

    問い合わせフォーム&メール送信機能

--- app_on_azure/application.py
+++ app_on_azure/application.py
@@ -1,20 +1,112 @@
-from flask import Flask, render_template, request
+import logging
+import os
+
+from email_validator import EmailNotValidError, validate_email
+from flask import (
+    Flask,
+    current_app,
+    flash,
+    g,
+    make_response,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
+# SECRET_KEYを追加する
+# app.config["SECRET_KEY"] = "2AZSMss3p5QPbcY2hBsJ"
+app.config["SECRET_KEY"] = "aaa"
 
-@app.route("/", methods=["GET", "POST"])
-def main_page():
-    if request.method == "GET":
-        text = "ここに結果が出力されます"
-        return render_template("page.html", text=text)
-    elif request.method == "POST":
-        name = request.form["name"]
-        hobby = request.form["hobby"]
-        text = f"こんにちは、{name}さん、あなたの趣味は{hobby}なんですね"
-        return render_template("page.html", text=text)
+# ログレベルを設定する
+app.logger.setLevel(logging.DEBUG)
+
+# リダイレクトを中断しないようにする
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+
+# Mailクラスのコンフィグを追加する
+app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER")
+app.config["MAIL_PORT"] = os.environ.get("MAIL_PORT")
+app.config["MAIL_USE_TLS"] = os.environ.get("MAIL_USE_TLS")
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER")
+
+# DebugToolbarExtensionにアプリケーションをセットする
+toolbar = DebugToolbarExtension(app)
+
+# flask-mail拡張を登録する
+mail = Mail(app)
 
 
-## 実行
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/contact/complete", methods=["GET", "POST"])
+def contact_complete():
+    if request.method == "POST":
+
+        # form属性を使ってフォームの値を取得する
+        username = request.form["username"]
+        email = request.form["email"]
+        description = request.form["description"]
+
+        # 入力チェック
+        is_valid = True
+        if not username:
+            flash("ユーザ名は必須です")
+            is_valid = False
+
+        if not email:
+            flash("メールアドレスは必須です")
+            is_valid = False
+
+        try:
+            validate_email(email)
+        except EmailNotValidError:
+            flash("メールアドレスの形式で入力してください")
+            is_valid = False
+
+        if not description:
+            flash("問い合わせ内容は必須です")
+            is_valid = False
+
+        if not is_valid:
+            return redirect(url_for("contact"))
+
+        # メールを送る
+        send_email(
+            email,
+            "問い合わせありがとうございました。",
+            "contact_mail",
+            username=username,
+            description=description,
+        )
+
+        # 問い合わせ完了エンドポイントへリダイレクトする
+        flash("問い合わせ内容はメールにて送信しました。問い合わせありがとうございます。")
+
+        # contactエンドポイントへリダイレクトする
+        return redirect(url_for("contact_complete"))
+
+    return render_template("contact_complete.html")
+
+
+def send_email(to, subject, template, **kwargs):
+    """メールを送信する関数"""
+    msg = Message(subject, recipients=[to])
+    msg.body = render_template(template + ".txt", **kwargs)
+    msg.html = render_template(template + ".html", **kwargs)
+    mail.send(msg)
+
+
+# 実行
 if __name__ == "__main__":
     app.run(debug=True)
