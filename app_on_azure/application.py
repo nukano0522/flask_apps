d1fc6fcd369088a3eb0880e737c210ce69616d94
Shuhei Nakano <nukano0522@gmail.com>
2022-07-29 22:39:49 +0900

    問い合わせフォーム&メール送信機能

--- app_on_azure/application.py
+++ app_on_azure/application.py
@@ -101,7 +101,7 @@ def contact_complete():
 
 def send_email(to, subject, template, **kwargs):
     """メールを送信する関数"""
-    msg = Message(subject, recipients=[to])
+    msg = Message(subject, sender=app.config["MAIL_DEFAULT_SENDER"], recipients=[to])
     msg.body = render_template(template + ".txt", **kwargs)
     msg.html = render_template(template + ".html", **kwargs)
     mail.send(msg)
