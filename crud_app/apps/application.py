d8ffa2a5e4faf4231d2c6237a23fdd40e9e1ea09
Shuhei Nakano <nukano0522@gmail.com>
2022-07-31 15:10:45 +0900

    FlaskとPostgreSQLによるCRUDアプリ

--- crud_app/apps/application.py
+++ crud_app/apps/application.py
@@ -1,16 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 import os
 # from apps.config import config
+
 db = SQLAlchemy()
+
+csrf = CSRFProtect()
 
 # create_app関数を作成する
 def create_app():
     # Flaskインスタンス生成
     app = Flask(__name__)
+    app.secret_key = "hogehoge"
     # app.config.from_object(config[config_key])
     # WEBSITE_HOSTNAME exists only in production environment
     if not 'WEBSITE_HOSTNAME' in os.environ:
@@ -26,8 +30,10 @@ def create_app():
     app.config.update(
         SQLALCHEMY_DATABASE_URI=app.config.get('DATABASE_URI'),
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        SQLALCHEMY_ECHO=True
+        SQLALCHEMY_ECHO=True,
+        WTF_CSRF_SECRET_KEY = "AuwzyszU5sugKN7KZs6f"
     )
+    csrf.init_app(app)
 
     db.init_app(app)
     Migrate(app, db)
--- crud_app/apps/crud/views.py
+++ crud_app/apps/crud/views.py
@@ -1,9 +1,8 @@
-# from apps.crud.forms import UserForm
+from .forms import UserForm
 from .models import User
 from flask import Blueprint, redirect, render_template, url_for
 # from flask_login import login_required
 
-# from apps.app import db
 import sys
 sys.path.append('../')
 from application import db
@@ -24,82 +23,69 @@ def index():
     return render_template("crud/index.html")
 
 
-@crud.route("/sql")
+# @crud.route("/sql")
+# # @login_required
+# def sql():
+#     db.session.query(User).all()
+#     return "コンソールログを確認してください"
+
+
+@crud.route("/users/new", methods=["GET", "POST"])
 # @login_required
-def sql():
-    db.session.query(User).all()
-    return "コンソールログを確認してください"
+def create_user():
+    # UserFormをインスタンス化する
+    form = UserForm()
+
+    # フォームの値をバリデートする
+    if form.validate_on_submit():
+        # ユーザーを作成する
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+        )
+
+        # ユーザーを追加してコミットする
+        db.session.add(user)
+        db.session.commit()
+
+        # ユーザーの一覧画面へリダイレクトする
+        return redirect(url_for("crud.users"))
+    return render_template("crud/create.html", form=form)
 
 
-@crud.route("/insert/<username>/<email>")
-def insert(username, email):
-    user = User(
-        username=username,
-        email=email,
-        password="password"
-    )
-    db.session.add(user)
-    db.session.commit()
-
-    return "aa"
-
-
-# @crud.route("/users/new", methods=["GET", "POST"])
+@crud.route("/users")
 # @login_required
-# def create_user():
-#     # UserFormをインスタンス化する
-#     form = UserForm()
-
-#     # フォームの値をバリデートする
-#     if form.validate_on_submit():
-#         # ユーザーを作成する
-#         user = User(
-#             username=form.username.data,
-#             email=form.email.data,
-#             password=form.password.data,
-#         )
-
-#         # ユーザーを追加してコミットする
-#         db.session.add(user)
-#         db.session.commit()
-
-#         # ユーザーの一覧画面へリダイレクトする
-#         return redirect(url_for("crud.users"))
-#     return render_template("crud/create.html", form=form)
+def users():
+    """ユーザーの一覧を取得する"""
+    users = User.query.all()
+    return render_template("crud/index.html", users=users)
 
 
-# @crud.route("/users")
+# methodsにGETとPOSTを指定する
+@crud.route("/users/<user_id>", methods=["GET", "POST"])
 # @login_required
-# def users():
-#     """ユーザーの一覧を取得する"""
-#     users = User.query.all()
-#     return render_template("crud/index.html", users=users)
+def edit_user(user_id):
+    form = UserForm()
 
+    # Userモデルを利用してユーザーを取得する
+    user = User.query.filter_by(id=user_id).first()
 
-# # methodsにGETとPOSTを指定する
-# @crud.route("/users/<user_id>", methods=["GET", "POST"])
-# @login_required
-# def edit_user(user_id):
-#     form = UserForm()
+    # formからサブミットされた場合はユーザーを更新しユーザーの一覧画面へリダイレクトする
+    if form.validate_on_submit():
+        user.username = form.username.data
+        user.email = form.email.data
+        user.password = form.password.data
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("crud.users"))
 
-#     # Userモデルを利用してユーザーを取得する
-#     user = User.query.filter_by(id=user_id).first()
-
-#     # formからサブミットされた場合はユーザーを更新しユーザーの一覧画面へリダイレクトする
-#     if form.validate_on_submit():
-#         user.username = form.username.data
-#         user.email = form.email.data
-#         user.password = form.password.data
-#         db.session.add(user)
-#         db.session.commit()
-#         return redirect(url_for("crud.users"))
-
-#     # GETの場合はHTMLを返す
-#     return render_template("crud/edit.html", user=user, form=form)
+    # GETの場合はHTMLを返す
+    return render_template("crud/edit.html", user=user, form=form)
 
 
 # @crud.route("/users/<user_id>/delete", methods=["POST"])
-# @login_required
+# # @login_required
 # def delete_user(user_id):
 #     user = User.query.filter_by(id=user_id).first()
 #     db.session.delete(user)
